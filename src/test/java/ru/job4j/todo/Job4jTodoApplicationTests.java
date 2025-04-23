1adcd2774b5f4e71fedd30f32bdfb9887ca807af
tasker312 <ivan255255@gmail.com>
2025-04-23 22:04:43 +0300

    init

---
+++ src/test/java/ru/job4j/todo/Job4jTodoApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.job4j.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class Job4jTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
